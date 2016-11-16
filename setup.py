11ce000156650247b52c63495279d4620528fd24
Takafumi Arakaki <aka.tkf@gmail.com>
2016-11-16 15:36:52 -0800

    Add minimal generated files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+import genrun
+
+setup(
+    name='genrun',
+    version=genrun.__version__,
+    py_modules=['genrun'],
+    author=genrun.__author__,
+    author_email='aka.tkf@gmail.com',
+    # url='https://github.com/tkf/genrun',
+    license=genrun.__license__,
+    # description='genrun - THIS DOES WHAT',
+    long_description=genrun.__doc__,
+    # keywords='KEYWORD, KEYWORD, KEYWORD',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    install_requires=[
+        # 'SOME_PACKAGE',
+    ],
+)
